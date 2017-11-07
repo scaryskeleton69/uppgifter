b6f7c9ca1bf23a8fd6674d3ea319a21701490c81
kevdah <kevin.dahlen@gmail.com>
2017-11-07 22:06:26 +0100

    uppgift2
    Uppgift 2
    En mer avancerad version av den originella uppgiften.

---
+++ uppgift2/uppgift2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           /* Console.WriteLine("Skriv två nummer");
+            Console.WriteLine("Första nummer: ");
+            string x1 = Console.ReadLine();
+            Console.WriteLine("Andra nummer: ");
+            string x2 = Console.ReadLine();
+
+            int z1 = Convert.ToInt32(x1);
+            int z2 = Convert.ToInt32(x2);
+            Console.WriteLine("Produkt: " + z1 * z2);
+            Console.WriteLine("Summa: " + z1 + z2);
+            Console.ReadLine();*/
+
+            #region v2
+            try
+            {
+                Console.WriteLine("Skriv två tal\nTal ett: ");
+                string n1 = Console.ReadLine();
+                Console.WriteLine("Tal två: ");
+                string n2 = Console.ReadLine();
+                //konverterar string till float
+                float x1 = float.Parse(n1);
+                float x2 = float.Parse(n2);
+                Console.WriteLine("Välj ett alternativ:\n1. Summa\n2. Produkt\n3. Båda");
+
+                string svar = Console.ReadLine();
+                //konverterar string till int
+                int svarint = Convert.ToInt32(svar);
+
+                if (svarint == 1)
+                {
+                    Console.WriteLine("Din summa är: " + (x1 + x2));
+                }
+                else if (svarint == 2)
+                {
+                    Console.WriteLine("Din produkt är: " + (x1 * x2));
+                }
+                else if (svarint == 3)
+                {
+                    Console.WriteLine("Produkt = " + (x1 * x2) + "\nSumma = " + (x1 + x2));
+                }
+                else
+                {
+                    Console.WriteLine("Du har valt fel nummer");
+                }
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Nu har du gjort något fel. Vänligen starta om programmet och gör rätt.");
+                Console.ReadLine();
+            }
+            #endregion
+        }
+    }
+}

0e6aba5263ad8e04deb805c2d26c32fa445b4b08
kevdah <kevin.dahlen@gmail.com>
2017-11-07 22:16:52 +0100

    uppgift1
    Första uppgiften - skriva ut en katt med Console.WriteLine();

---
+++ uppgift1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n	   \" \" \" \" \" ");
+            Console.WriteLine(@"     \ \  	       / /");
+            Console.WriteLine("	O  	      O ");
+            Console.WriteLine(@"         \/\/\/\/\/\/\ ");
+            Console.WriteLine("            _______");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}

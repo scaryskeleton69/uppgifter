390ba1450941db1418964c88c210553918d567a1
kevdah <kevin.dahlen@gmail.com>
2017-11-14 10:05:44 +0100

    Uppgift5
    En uppgift som kollar om ett år är ett skottår eller inte

---
+++ uppgift5/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett år: ");
+            float year = float.Parse(Console.ReadLine());
+            //skottår är delbara med 4
+            //för att bestämma om ett tal är delbart med ett annat kan man använda "%" modulus operatorn
+            Console.WriteLine(5 % 20);
+            //den matematiska beräkningen x modulus n = 0 betyder att x är jämnt delbart med n
+            if (year % 4 == 0) //om år är jämnt delbart med 4 är det ett skottår
+            {
+                Console.WriteLine("Ditt år är ett skottår");
+            }
+            else
+            {
+                Console.WriteLine("Ditt år är inte ett skottår");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}

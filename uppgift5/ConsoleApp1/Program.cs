e61ec6c3eaf827bc23cfdffb85992871200fb4b5
kevdah <kevin.dahlen@gmail.com>
2017-11-16 09:06:46 +0100

    Bugfix v2 - finare kod

--- uppgift5/ConsoleApp1/Program.cs
+++ uppgift5/ConsoleApp1/Program.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Skottår
+namespace ConsoleApp1
 {
     class Program
     {
@@ -14,36 +15,24 @@ namespace Skottår
             {
                 Console.WriteLine("Skriv in ett år: ");
                 float year = float.Parse(Console.ReadLine());
-
-                if (year % 100 == 0)
-                {
-                    if (year % 400 == 0)
-                    {
-                        Console.WriteLine("Ditt år är ett skottår");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ditt år är inte ett skottår");
-                    }
-
-                }
-
-                else if (year % 4 == 0 && year % 100 != 0)
+                //skottår är delbara med 4, eller 100 och 400, men inte 100.
+                //den matematiska beräkningen x modulus n = 0 betyder att x är jämnt delbart med n
+                if ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
                 {
                     Console.WriteLine("Ditt år är ett skottår");
                 }
                 else
                 {
-                    Console.WriteLine("Ditt år är inte ett skottår");
+                    Console.WriteLine("Ditt år är inte ett skottår\n");
                 }
-
                 Console.ReadLine();
             }
             catch
             {
-                Console.WriteLine("Någonting gick fel. Dubbelkolla så att du skrev in årtalet rätt. \nProgrammet stängs nu...");
-                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("Se till så att du skrev in årtalet rätt!\nProgrammet avslutas om 3 sekunder");
+                Thread.Sleep(3000);
             }
+            
         }
     }
 }
